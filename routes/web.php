83dbdd4f71aed1e33dcc793470d41273a7bbfdf2
democoder91 <Mm0102900192>
2025-10-13 14:02:40 +0300

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use NetSuite\Classes\Account;
+use NetSuite\Classes\AsyncGetListRequest;
+use NetSuite\Classes\GetAllRequest;
+use NetSuite\Classes\GetListRequest;
+use NetSuite\Classes\GetRequest;
+use NetSuite\Classes\RecordRef;
+use NetSuite\Classes\RecordType;
+use NetSuite\Classes\VendorReturnAuthorization;
+use NetSuite\NetSuiteService;
+
+Route::get('/', function () {
+    $service = new NetSuiteService();
+
+    // Example: Fetch all accounts
+//    $getAllRequest = new GetRequest();
+//    $getAllRequest->baseRef = new RecordRef();
+//    $getAllRequest->baseRef->type = RecordType::account;
+//    // id
+//    $getAllRequest->baseRef->internalId = '1'; // Replace with a valid internal ID
+//    $getAllResponse = $service->get($getAllRequest);
+
+    $getAllRequest = new GetAllRequest();
+    $getAllRequest->record = new RecordRef();
+    $getAllRequest->record->recordType = RecordType::account;
+    $getAllResponse = $service->getAll($getAllRequest);
+
+
+
+    dd($getAllResponse);
+
+});
